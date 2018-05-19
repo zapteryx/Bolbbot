d5f309d724820e220b811f26f5e121b3bcf98126
Yenaman <33003685+Yenaman@users.noreply.github.com>
2018-05-19 18:30:48 +0200

    Constants updated with new Servers/Channels IDs

--- src/main/java/com/github/donotspampls/bolbbot/Constants.java
+++ src/main/java/com/github/donotspampls/bolbbot/Constants.java
@@ -3,23 +3,23 @@ package com.github.donotspampls.bolbbot;
 public class Constants {
 
     // Guilds
-    public static final String BOLB_CHAIRS_HUB = "427706965855305738";
-    public static final String BOLB_CHAIRS_1 = "427707015717191680";
-    public static final String BOLB_CHAIRS_2 = "427707071149113345";
-    public static final String BOLB_CHAIRS_3 = "427707230461362185";
-    public static final String BOLB_CHAIRS_4 = "427707287155638272";
-    public static final String BOLB_CHAIRS_5 = "427707330197716992";
-    public static final String BOLB_CHAIRS_6 = "427707375978283009";
+    public static final String BOLB_CHAIRS_HUB = "445256982040018964";
+    public static final String BOLB_CHAIRS_1 = "445257017687539713";
+    public static final String BOLB_CHAIRS_2 = "445257039183216661";
+    public static final String BOLB_CHAIRS_3 = "445257054605541389";
+    public static final String BOLB_CHAIRS_4 = "445257077578006539";
+    public static final String BOLB_CHAIRS_5 = "445257303369711626";
+    public static final String BOLB_CHAIRS_6 = "445260596410318858";
 
     // Channels
-    public static final String GAME_LOGS = "427782433744748554";
-    public static final String BC1_INFO = "427734182836568064";
-    public static final String BC1_GENERAL = "427707015717191682";
-    public static final String BC2_INFO = "427767704024383488";
-    public static final String BC2_GENERAL = "427707071149113348";
-    public static final String BC3_GENERAL = "427707230461362187";
-    public static final String BC4_GENERAL = "427707287155638274";
-    public static final String BC5_GENERAL = "427707330197716994";
-    public static final String BC6_GENERAL = "427707375978283013";
+    public static final String GAME_LOGS = "445263102603427840";
+    public static final String BC1_INFO = "445272991560105984";
+    public static final String BC1_GENERAL = "445257017687539715";
+    public static final String BC2_INFO = "445273087622250507";
+    public static final String BC2_GENERAL = "445257039732539404";
+    public static final String BC3_GENERAL = "445257054605541393";
+    public static final String BC4_GENERAL = "445257078173466644";
+    public static final String BC5_GENERAL = "445257303369711629";
+    public static final String BC6_GENERAL = "445260596410318862";
 
 }
